9fc00a2d3ad4db71269b42fbafc85f5415c17bc1
Tushar <toshuu.ps4@gmail.com>
2019-11-04 10:37:14 +0530

    wiz

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
