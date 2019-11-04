a3bc7835625ce95c33f55488de1c6e9ceba11714
Tushar <toshuu.ps4@gmail.com>
2019-11-04 11:02:26 +0530

    port from env var

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const Datastore = require('nedb');
+require('dotenv').config();
+
 
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('listening at 3000'));
+app.listen(port, () => {
+	console.log('listening at ${port}');
+});
+	
+	
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
